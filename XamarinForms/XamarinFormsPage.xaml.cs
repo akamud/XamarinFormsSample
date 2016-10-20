4dc9414391671747c1cc6e09f8bf536895275a3e
Mahmoud Ali <muddibr@gmail.com>
2016-10-20 08:43:06 -0200

    Initial commit

---
+++ XamarinForms/XamarinFormsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public partial class XamarinFormsPage : ContentPage
+    {
+        public XamarinFormsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
